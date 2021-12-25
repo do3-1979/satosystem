9de168d7ca69054ba63e30ef0800421ca876a5d7
do3-1979 <70438376+do3-1979@users.noreply.github.com>
2021-12-26 00:06:23 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # APIキーを設定
-apiKey = 'd3sX9AUNdVEzLhabgmBGy0qg'
+apiKey = ''
 
 # APIシークレットを設定
-secret = 'n0mnJgflxnaF_IyAtH8E9iA-BUJr6D05yTlPjKCSWuR36pOu'
+secret = ''
 
 # 通知用LINEトークン
-line_token = "hCddmAoaCRUBKG59Yf3BlK1KBHrwkOFelehDxzqraFl"
+line_token = ""
