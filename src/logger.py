69b8a4afef10c36d4583005cf356081f61bae1d6
Satoshi Yamada <satoshi.yamada.79@gmail.com>
2023-09-24 21:03:54 +0900

    add uitl class

--- src/logger.py
+++ src/logger.py
@@ -11,13 +11,8 @@ Loggerクラスのインスタンスを作成し、log() メソッドと log_err
 import os
 import json
 import zipfile
-import pandas as pd
 from datetime import datetime
 import logging
-import openpyxl
-from openpyxl import Workbook
-from openpyxl.utils.dataframe import dataframe_to_rows
-from openpyxl.chart import LineChart, Reference
 
 class Logger:
     _instance = None  # シングルトンインスタンスを格納するクラス変数
@@ -125,152 +120,41 @@ class Logger:
             else:
                 log_count += 1
 
-    def extract_and_export_logs(self, log_directory, num_logs, output_excel_file):
-        """
-        指定された数の圧縮ログファイルを読み込み、エクセルファイルにデータとグラフを出力します。
-
-        Args:
-            log_directory (str): 圧縮ログファイルが格納されているディレクトリ
-            num_logs (int): 読み込むログファイルの数
-            output_excel_file (str): 出力するエクセルファイルの名前
-        """
-        # 圧縮ログファイルのリストを作成
-        log_files = []
-        for root, _, files in os.walk(log_directory):
-            for file in files:
-                if file.endswith(".zip"):
-                    log_files.append(os.path.join(root, file))
-
-        # 新しいものから指定された数のログファイルを選択
-        selected_log_files = sorted(log_files, reverse=True)[:num_logs]
-
-        # データを格納するためのリスト
-        data = []
-
-        # 選択されたログファイルからデータを抽出
-        for log_file in selected_log_files:
-            with zipfile.ZipFile(log_file, "r") as log_zip:
-                with log_zip.open(log_zip.namelist()[0]) as log_json:
-                    log_data = pd.read_json(log_json)
-                    data.append(log_data)
-
-        # データを連結
-        combined_data = pd.concat(data, ignore_index=True)
-
-        # エクセルファイルに出力
-        output_excel_file_path = os.path.join(log_directory, output_excel_file)
-        workbook = Workbook()
-        writer = pd.ExcelWriter(output_excel_file_path, engine='openpyxl')
-        writer.book = workbook
-
-        # データをエクセルに書き込み
-        combined_data.to_excel(writer, sheet_name='Data', index=False)
-
-        # 必要なカラムを選択
-        """
-        selected_columns = [
-            "close_time_dt",
-            "open_price",
-            "high_price",
-            "low_price",
-            "close_price",
-            "Volume",
-            "chart_time",
-            "stop_price",
-            "position_size",
-            "total_size",
-            "volatility",
-            "decision",
-            "side",
-            "order_type"
-        ]
-        """
-
-        """
-        # カラムごとに折れ線グラフを生成し、別のシートに追加
-        selected_graph_columns = [
-            "open_price",
-            "high_price",
-            "low_price",
-            "close_price",
-            "Volume",
-            "volatility",
-            "position_size",
-            "total_size"
-        ]
-        """
-
-        column_name = "グラフ"
-        chart_sheet = workbook.create_sheet(title=f"Graph")
-        self.generate_line_chart(combined_data, column_name, chart_sheet)
-
-        # エクセルファイルを保存
-        writer.save()
-        writer.close()
-        
-    def generate_line_chart(self, data, column_name, chart_sheet):
-        """
-        データから指定されたカラムの折れ線グラフを生成し、指定されたシートに追加します。
-
-        Args:
-            data (DataFrame): グラフを生成するデータ
-            column_name (str): グラフを生成するカラムの名前
-            chart_sheet (Worksheet): グラフを追加するシート
-        """
-        chart = LineChart()
-        chart.title = column_name
-        chart.y_axis.title = column_name
-        chart.x_axis.title = "chart_time"
-
-        data_rows = list(dataframe_to_rows(data, index=False, header=True))
-        labels = Reference(chart_sheet, min_col=1, min_row=2, max_row=len(data_rows), max_col=1)
-        values = Reference(chart_sheet, min_col=2, min_row=1, max_row=len(data_rows), max_col=len(data_rows[0]))
-
-        chart.add_data(values, titles_from_data=True)
-        chart.set_categories(labels)
-
-        chart_sheet.add_chart(chart, f"D{len(data_rows) + 3}")
-
 if __name__ == "__main__":
     # Loggerクラスの初期化
     logger = Logger()
 
-    """
-
-    i = 1
     trade_data = {
-        "chart_time": "2023-09-09 14:30:00",
-        "time": "2023-09-09 14:31:00",
-        "high_price": 150.0 + i,
-        "low_price": 140.0 - i,
-        "price": 145.0 + i,
-        "stop_price": 140.0,
-        "volume": 100,
-        "total_quantity": 1000 + i,
-        "position_quantity": 500,
-        "side": "BUY",
-        "donchian": {"signal": True, "value": 145.0},
-        "pvo": {"signal": True, "side": "BUY"}
-    }
+            "close_time": 1695549600.0,
+            "close_time_dt": "2023/09/24 19:00",
+            "open_price": 26586.0,
+            "high_price": 26586.5,
+            "low_price": 26576.0,
+            "close_price": 26579.5,
+            "Volume": 25.59252078,
+            "real_time": "2023/09/24 20:13:02",
+            "stop_price": 0,
+            "position_size": 0,
+            "total_size": 0,
+            "profit_and_loss": 0,
+            "volatility": 28,
+            "decision": None,
+            "side": None,
+            "order_type": "Market",
+            "donchian": {
+                "signal": False,
+                "side": None
+            },
+            "pvo": {
+                "signal": False,
+                "side": None
+            }
+        }
 
     # 取引データを記録
     logger.log_trade_data(trade_data)
 
-    # 1週間ごとにファイルを分けるかチェック
-    current_time = datetime.now()
-    #if current_time.strftime("%w") == "0":  # 0は日曜日を表す
-    #logger.compress_logs()  # 圧縮
+    logger.close_log_file()
+    
+    logger.compress_logs()
 
-    time.sleep(1)
-
-    # ログをローテート
-    logger.rotate_logs()
-
-    """
-
-    # 使用例
-    log_directory = "logs"  # ログファイルのディレクトリ
-    num_logs_to_read = 5  # 読み込むログファイルの数
-    output_excel_file = "combined_logs.xlsx"  # 出力エクセルファイルの名前
-
-    logger.extract_and_export_logs(log_directory, num_logs_to_read, output_excel_file)
---
+++ src/util.py
@@ -0,0 +1,131 @@
+import os
+import json
+import zipfile
+import pandas as pd
+from datetime import datetime
+import logging
+import openpyxl
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.chart import LineChart, Reference
+
+class Util:
+    def extract_and_export_logs(self, log_directory, num_logs, output_excel_file):
+        """
+        指定された数の圧縮ログファイルを読み込み、エクセルファイルにデータとグラフを出力します。
+
+        Args:
+            log_directory (str): 圧縮ログファイルが格納されているディレクトリ
+            num_logs (int): 読み込むログファイルの数
+            output_excel_file (str): 出力するエクセルファイルの名前
+        """
+        # 圧縮ログファイルのリストを作成
+        log_files = []
+        for root, _, files in os.walk(log_directory):
+            for file in files:
+                if file.endswith(".zip"):
+                    log_files.append(os.path.join(root, file))
+
+        # 新しいものから指定された数のログファイルを選択
+        selected_log_files = sorted(log_files, reverse=True)[:num_logs]
+
+        print(selected_log_files)
+
+        # データを格納するためのリスト
+        data = []
+
+        # 選択されたログファイルからデータを抽出
+        for log_file in selected_log_files:
+            with zipfile.ZipFile(log_file, "r") as log_zip:
+                with log_zip.open(log_zip.namelist()[0]) as log_json:
+                    log_data = pd.read_json(log_json)
+                    data.append(log_data)
+
+        # データを連結
+        combined_data = pd.concat(data, ignore_index=True)
+
+        # エクセルファイルに出力
+        output_excel_file_path = os.path.join(log_directory, output_excel_file)
+        workbook = Workbook()
+        writer = pd.ExcelWriter(output_excel_file_path, engine='openpyxl')
+        writer.book = workbook
+
+        # データをエクセルに書き込み
+        combined_data.to_excel(writer, sheet_name='Data', index=False)
+
+        # 必要なカラムを選択
+        """
+        selected_columns = [
+            "close_time_dt",
+            "open_price",
+            "high_price",
+            "low_price",
+            "close_price",
+            "Volume",
+            "chart_time",
+            "stop_price",
+            "position_size",
+            "total_size",
+            "volatility",
+            "decision",
+            "side",
+            "order_type"
+        ]
+        """
+
+        """
+        # カラムごとに折れ線グラフを生成し、別のシートに追加
+        selected_graph_columns = [
+            "open_price",
+            "high_price",
+            "low_price",
+            "close_price",
+            "Volume",
+            "volatility",
+            "position_size",
+            "total_size"
+        ]
+        """
+
+        column_name = "グラフ"
+        chart_sheet = workbook.create_sheet(title=f"Graph")
+        self.generate_line_chart(combined_data, column_name, chart_sheet)
+
+        # エクセルファイルを保存
+        writer.save()
+        writer.close()
+        
+    def generate_line_chart(self, data, column_name, chart_sheet):
+        """
+        データから指定されたカラムの折れ線グラフを生成し、指定されたシートに追加します。
+
+        Args:
+            data (DataFrame): グラフを生成するデータ
+            column_name (str): グラフを生成するカラムの名前
+            chart_sheet (Worksheet): グラフを追加するシート
+        """
+        chart = LineChart()
+        chart.title = column_name
+        chart.y_axis.title = column_name
+        chart.x_axis.title = "chart_time"
+
+        data_rows = list(dataframe_to_rows(data, index=False, header=True))
+        labels = Reference(chart_sheet, min_col=1, min_row=2, max_row=len(data_rows), max_col=1)
+        values = Reference(chart_sheet, min_col=2, min_row=1, max_row=len(data_rows), max_col=len(data_rows[0]))
+
+        chart.add_data(values, titles_from_data=True)
+        chart.set_categories(labels)
+
+        chart_sheet.add_chart(chart, f"D{len(data_rows) + 3}")
+
+if __name__ == "__main__":
+    # Loggerクラスの初期化
+    util = Util()
+
+    # 使用例
+    #log_directory = "logs"  # ログファイルのディレクトリ
+    log_directory = "../test/test_data"  # ログファイルのディレクトリ
+    num_logs_to_read = 10  # 読み込むログファイルの数
+    output_excel_file = "combined_logs.xlsx"  # 出力エクセルファイルの名前
+
+    util.extract_and_export_logs(log_directory, num_logs_to_read, output_excel_file)
