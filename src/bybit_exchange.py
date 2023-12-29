c821a732af5d35572dd6bf1e091a30531ca9ccb4
Satoshi Yamada <satoshi.yamada.79@gmail.com>
2023-12-30 00:50:21 +0900

    update backtest

--- src/bybit_exchange.py
+++ src/bybit_exchange.py
@@ -254,7 +254,7 @@ class BybitExchange(Exchange):
 
         return epoch_time
 
-    def fetch_ohlcv(self, start_epoch, end_epoch):
+    def fetch_ohlcv(self, start_epoch, end_epoch, time_frame):
         """
         取引情報を取得
 
@@ -264,10 +264,12 @@ class BybitExchange(Exchange):
         err_occuerd = False
         ohlcv_data = []
 
+        back_test_mode = Config.get_back_test_mode()
         server_retry_wait = Config.get_server_retry_wait()
         
         # 終端時間の計算
         end_epoch_fixed = self.get_nearest_epoch_time(end_epoch)
+        total_progress = int((end_epoch_fixed - start_epoch) / 60) + 1  # 1分足なので1分ごとに進捗
 
         get_time = start_epoch
         while get_time < end_epoch_fixed:
@@ -276,7 +278,7 @@ class BybitExchange(Exchange):
                 try:
                     ohlcv = self.exchange.fetch_ohlcv(
                         symbol = self.market,
-                        timeframe = self.timeframe,
+                        timeframe = time_frame,
                         since = int(get_time * 1000), # bybitはミリ秒なので1000倍
                     )
                     break
@@ -308,78 +310,21 @@ class BybitExchange(Exchange):
                 else:
                     break
             get_time = tmp_time
-        # TODO 取得データ確認（抜け漏れ、ダブり）
+            
+            if back_test_mode == 1:
+                progress = int((get_time - start_epoch) / 60)
+                start_date = datetime.fromtimestamp(start_epoch).strftime('%Y/%m/%d %H:%M:%S')
+                end_date = datetime.fromtimestamp(end_epoch_fixed).strftime('%Y/%m/%d %H:%M:%S')
+                get_date = datetime.fromtimestamp(get_time).strftime('%Y/%m/%d %H:%M:%S')
+                print(f"進捗：{progress}/{total_progress} 開始: {start_date} 終了：{end_date} 処理中: {get_date}", end='\r')
 
-        return ohlcv_data
-    
-    def fetch_ohlcv_by_minutes(self, start_epoch, end_epoch):
-        ohlcv_data = []
-        err_occurred = False
-        server_retry_wait = Config.get_server_retry_wait()
-        progress = 0
-
-        # 終端時間の計算
-        end_epoch_fixed = self.get_nearest_epoch_time(end_epoch)
-        total_progress = int((end_epoch_fixed - start_epoch) / 60) + 1  # 1分足なので1分ごとに進捗
-
-        get_time = start_epoch
-        while get_time < end_epoch_fixed:
-            # 価格取得
-            while True:
-                try:
-                    ohlcv = self.exchange.fetch_ohlcv(
-                        symbol=self.market,
-                        timeframe=1, # 1分固定
-                        since=int(get_time * 1000),  # bybitはミリ秒なので1000倍
-                    )
-                    break
-                
-                except ccxt.BaseError as e:
-                    if err_occurred is False:
-                        self.logger.log_error(f"1分価格取得エラー:{str(e)}")
-                        err_occurred = True
-                    time.sleep(server_retry_wait)
-
-            if err_occurred is True:
-                self.logger.log_error("1分価格取得エラー復帰")
-
-            # データ成型
-            for i in range(len(ohlcv)):
-                tmp_time = ohlcv[i][0] / 1000
-                if tmp_time < end_epoch_fixed:
-                    # volumeは0もありうるので除外する
-                    if ohlcv[i][1] != 0 and \
-                            ohlcv[i][2] != 0 and \
-                            ohlcv[i][3] != 0 and \
-                            ohlcv[i][4] != 0:
-                        ohlcv_data.append({
-                            "close_time": tmp_time,
-                            "close_time_dt": datetime.fromtimestamp(tmp_time).strftime('%Y/%m/%d %H:%M'),
-                            "open_price": ohlcv[i][1],
-                            "high_price": ohlcv[i][2],
-                            "low_price": ohlcv[i][3],
-                            "close_price": ohlcv[i][4],
-                            "Volume": ohlcv[i][5]
-                        })
-                    else:
-                        break
-                get_time = tmp_time
-                # print(f"進捗：{progress}/{total_progress} start_epoch: {start_epoch} end_epoch: {end_epoch_fixed} progress_time: {get_time}")
-
-            # 進捗表示
-            progress = int((get_time - start_epoch) / 60)
-            start_date = datetime.fromtimestamp(start_epoch).strftime('%Y/%m/%d %H:%M:%S')
-            end_date = datetime.fromtimestamp(end_epoch_fixed).strftime('%Y/%m/%d %H:%M:%S')
-            get_date = datetime.fromtimestamp(get_time).strftime('%Y/%m/%d %H:%M:%S')
-            print(f"進捗：{progress}/{total_progress} 開始: {start_date} 終了：{end_date} 処理中: {get_date}", end='\r')
-
-        print("")
-
+        if back_test_mode == 1:
+            print("")
         # TODO 取得データ確認（抜け漏れ、ダブり）
 
         return ohlcv_data
 
-    def fetch_latest_ohlcv(self):
+    def fetch_latest_ohlcv(self, time_frame):
         """
         最新取引情報を取得
 
@@ -395,7 +340,7 @@ class BybitExchange(Exchange):
             try:
                 ohlcv = self.exchange.fetch_ohlcv(
                     symbol = self.market,
-                    timeframe = self.timeframe,
+                    timeframe = time_frame,
                 )
                 break
             except ccxt.BaseError as e:
@@ -467,14 +412,15 @@ if __name__ == "__main__":
     print(f"{Config.get_market()} の最新価格情報")
     price = exchange.fetch_ticker()
     print(f"価格: {price}")
+    balance_tether = round(price * balance)
+    print(f"資産[BTC/USD]: {balance_tether}")
     print("----------")
 
-    print("最新価格データを取得")
+    print("最新価格データ 2h を取得")
     print("----------")
     start_price_time = time.time()
-    ohlcv_data = exchange.fetch_latest_ohlcv()
+    ohlcv_data = exchange.fetch_latest_ohlcv(120)
     end_price_time = time.time()
-
     entry = ohlcv_data[0]
     print(f"時刻: {entry['close_time_dt']}")
     print(f"開始価格: {entry['open_price']}")
@@ -484,13 +430,73 @@ if __name__ == "__main__":
     print(f"出来高: {entry['Volume']}")
     print("----------")
 
-    print("価格データを取得")
+    print("最新価格データ 15m を取得")
+    print("----------")
+    start_price_time = time.time()
+    ohlcv_data = exchange.fetch_latest_ohlcv(15)
+    end_price_time = time.time()
+    entry = ohlcv_data[0]
+    print(f"時刻: {entry['close_time_dt']}")
+    print(f"開始価格: {entry['open_price']}")
+    print(f"最高価格: {entry['high_price']}")
+    print(f"最低価格: {entry['low_price']}")
+    print(f"終値: {entry['close_price']}")
+    print(f"出来高: {entry['Volume']}")
+    print("----------")
+    
+    print("最新価格データ 1m を取得")
+    print("----------")
+    start_price_time = time.time()
+    ohlcv_data = exchange.fetch_latest_ohlcv(1)
+    end_price_time = time.time()
+    entry = ohlcv_data[0]
+    print(f"時刻: {entry['close_time_dt']}")
+    print(f"開始価格: {entry['open_price']}")
+    print(f"最高価格: {entry['high_price']}")
+    print(f"最低価格: {entry['low_price']}")
+    print(f"終値: {entry['close_price']}")
+    print(f"出来高: {entry['Volume']}")
+    print("----------")
+
+    print("価格データを取得 2h")
     print("----------")
     start_price_time = time.time()
     start_epoch = Config.get_start_epoch()
     end_epoch = Config.get_end_epoch()
-    ohlcv_data = exchange.fetch_ohlcv(start_epoch,end_epoch)
+    ohlcv_data = exchange.fetch_ohlcv(start_epoch,end_epoch,120)
     end_price_time = time.time()
+    
+    data_num = len(ohlcv_data)
+    print(f"データ数: {data_num}")
+    
+    # 表示するエントリーのインデックスを指定
+    selected_entries = [0, -1]  # 最初と最後
+
+    # 選択したエントリーを表示
+    for index in selected_entries:
+        entry = ohlcv_data[index]
+        print(f"選択した価格データ:{index}")
+        print(f"時刻: {entry['close_time_dt']}")
+        print(f"開始価格: {entry['open_price']}")
+        print(f"最高価格: {entry['high_price']}")
+        print(f"最低価格: {entry['low_price']}")
+        print(f"終値: {entry['close_price']}")
+        print(f"出来高: {entry['Volume']}")
+        print("----------")
+
+    print("口座残高情報取得にかかった時間: {:.2f}秒".format(end_balance_time - start_balance_time))
+    print("価格データ取得にかかった時間: {:.2f}秒".format(end_price_time - start_price_time))
+    print("----------")
+    print("価格データを取得 15 min")
+    print("----------")
+    start_price_time = time.time()
+    start_epoch = Config.get_start_epoch()
+    end_epoch = Config.get_end_epoch()
+    ohlcv_data = exchange.fetch_ohlcv(start_epoch,end_epoch,15)
+    end_price_time = time.time()
+
+    data_num = len(ohlcv_data)
+    print(f"データ数: {data_num}")
     
     # 表示するエントリーのインデックスを指定
     selected_entries = [0, -1]  # 最初と最後
